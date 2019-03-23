ebe88e83f203befa2e0ec4fb9df8ee4f769081fe
root <root@localhost.localdomain>
2019-03-23 15:23:14 -0300

    >DEDSEC:/

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "<================DEDSEC==================>"
+apt-get install python
+apt-get install websploit
+pip install pyinstaller
+apt-get install netcat
+echo "<================DEDSEC==================>"
+
